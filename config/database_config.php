9fbeae8fe4c9ac5afb248c400f66695e919f4bf6
Will Patton <willpatton@users.noreply.github.com>
2018-05-02 11:59:08 -0400

    Update database_config.php
    refactoring

--- config/database_config.php
+++ config/database_config.php
@@ -7,17 +7,16 @@
 $db_config['host'] = 'localhost';
 $db_config['database'] = "hackdmc";
 $db_config['username'] = "root";
-$db_config['password'] = "mmi1and1";
+$db_config['password'] = "";
 $db_config['port'] = 3306;
 $db_config['socket'] = '';
 
 //REMOTE
-//if (\Dynamics\App::isRemote()) {
 if(isRemote()) {
     $db_config['host'] = "localhost";
-    $db_config['database'] = "db638805905";
-    $db_config['username'] = "dbo638805905";
-    $db_config['password'] = "Mmi1and1!";
+    $db_config['database'] = "hackdmc";
+    $db_config['username'] = "root";
+    $db_config['password'] = "";
     $db_config['port'] = 3306;
-    $db_config['socket'] = "/tmp/mysql5.sock";
+    $db_config['socket'] = '';
 }
